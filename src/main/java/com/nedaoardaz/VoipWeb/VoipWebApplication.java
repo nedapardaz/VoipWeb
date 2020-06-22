6db4c042be3c944a356be3b5c3229eff154f8bcf
h.khoshdaman <khoshdaman_20@yahoo.com>
2020-06-22 15:03:43 +0330

    Create Voip Web Projects

---
+++ src/main/java/com/nedaoardaz/VoipWeb/VoipWebApplication.java
@@ -0,0 +1,13 @@
+package com.nedaoardaz.VoipWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoipWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoipWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nedaoardaz/VoipWeb/VoipWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nedaoardaz.VoipWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoipWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
